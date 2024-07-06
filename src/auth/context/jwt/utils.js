2f5f4fd6bf2206194c1de4b5b06b50bcc952bc8b
Jack Hsu <contactjackhsu@gmail.com>
2024-07-06 23:24:14 +0800

    add refresh token utils

--- src/auth/context/jwt/utils.js
+++ src/auth/context/jwt/utils.js
@@ -1,4 +1,5 @@
-import axios from 'src/utils/axios';
+import axios, { endpoints } from 'src/utils/axios';
+import { v4 as uuidv4 } from 'uuid';
 
 // ----------------------------------------------------------------------
 
@@ -52,10 +53,15 @@ export const tokenExpired = (exp, logout) => {
 
 // ----------------------------------------------------------------------
 
-export const setSession = (accessToken, logout) => {
+export const setSession = (accessToken, logout, refreshToken) => {
   if (accessToken) {
     sessionStorage.setItem('accessToken', accessToken);
 
+    if (refreshToken) {
+      const expires = new Date();
+      expires.setTime(expires.getTime() + 1 * 24 * 60 * 60 * 1000);
+      document.cookie = `refreshToken=${refreshToken}; path=/; SameSite=Strict; expires=${expires.toUTCString()}`;
+    }
     axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 
     // This function below will handle when token is expired
@@ -65,7 +71,39 @@ export const setSession = (accessToken, logout) => {
   } else {
     sessionStorage.removeItem('accessToken');
     sessionStorage.removeItem('user');
+    document.cookie = 'refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
 
     delete axios.defaults.headers.common.Authorization;
+  }
+};
+
+// a function that simulates refresh token generation, normally this would be done on the server.
+// The basic flow is for server to send an access token with a short expiration time and a refresh token with a long expiration time.
+// and client stores the refresh token in a secure place (like httpOnly cookie) and the access token in session storage.
+export const generateRefreshToken = () => {
+  return uuidv4();
+};
+
+const getRefreshTokenFromCookie = () => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; refreshToken=`);
+  const token = parts.length === 2 ? parts.pop().split(';').shift() : null;
+  return token;
+};
+
+export const getRefreshToken = async () => {
+  const accessToken = sessionStorage.getItem('accessToken');
+  try {
+    const response = await axios.post(
+      endpoints.token.refresh,
+      {
+        refreshToken: accessToken,
+      },
+      { skipAuthRefresh: true }
+    );
+    return response.data;
+  } catch (error) {
+    console.error('Error refreshing token:', error);
+    return null;
   }
 };
