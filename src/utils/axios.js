ac92b568a0b69922c4e0c680c5b8ecbe1e4357d4
Jack Hsu <contactjackhsu@gmail.com>
2024-07-07 13:37:58 +0800

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 import { HOST_API } from 'src/config-global';
 import { TEST_API } from 'src/config-global';
+import { getRefreshToken, setSession } from 'src/auth/context/jwt/utils';
 
 // ----------------------------------------------------------------------
 
@@ -9,7 +10,32 @@ const axiosInstance = axios.create({ baseURL: TEST_API, withCredentials: true })
 
 axiosInstance.interceptors.response.use(
   (res) => res,
-  (error) => Promise.reject((error.response && error.response.data) || 'Something went wrong')
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      (error.response.status === 401 && !originalRequest._retry) ||
+      (error.response.status === 403 && !originalRequest._retry)
+    ) {
+      console.log('axios trying to refresh token');
+      originalRequest._retry = true;
+      try {
+        const { accessToken } = await getRefreshToken();
+        setSession(accessToken);
+
+        // Update the Authorization header with the new token
+        axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+        originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+
+        // Retry the original request with the new token
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        return Promise.reject(err);
+      }
+    }
+
+    return Promise.reject((error.response && error.response.data) || 'Something went wrong');
+  }
 );
 
 export default axiosInstance;
